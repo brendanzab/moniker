764c5a30f40f2efba5b099aa2cf5cb3c4ff8e50d
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2018-07-02 16:15:08 +1000

    Add evaluator to stlc example

--- moniker/examples/stlc.rs
+++ moniker/examples/stlc.rs
@@ -27,6 +27,41 @@ pub enum Expr {
     App(Rc<Expr>, Rc<Expr>),
 }
 
+// FIXME: auto-derive this somehow!
+fn subst(expr: &Rc<Expr>, subst_name: &FreeVar, subst_expr: &Rc<Expr>) -> Rc<Expr> {
+    match **expr {
+        Expr::Ann(ref expr, ref ty) => {
+            Rc::new(Expr::Ann(subst(expr, subst_name, subst_expr), ty.clone()))
+        },
+        Expr::Var(Var::Free(ref n)) if subst_name == n => subst_expr.clone(),
+        Expr::Var(_) => expr.clone(),
+        Expr::Lam(ref scope) => Rc::new(Expr::Lam(Scope {
+            unsafe_pattern: scope.unsafe_pattern.clone(),
+            unsafe_body: subst(&scope.unsafe_body, subst_name, subst_expr),
+        })),
+        Expr::App(ref fun, ref arg) => Rc::new(Expr::App(
+            subst(fun, subst_name, subst_expr),
+            subst(arg, subst_name, subst_expr),
+        )),
+    }
+}
+
+pub fn eval(expr: &Rc<Expr>) -> Rc<Expr> {
+    match **expr {
+        Expr::Ann(ref expr, _) => eval(expr),
+        Expr::Var(Var::Free(_)) => expr.clone(),
+        Expr::Var(Var::Bound(ref name, _)) => panic!("encountered a bound variable: {:?}", name),
+        Expr::Lam(_) => expr.clone(),
+        Expr::App(ref fun, ref arg) => match *eval(fun) {
+            Expr::Lam(ref scope) => {
+                let ((name, _), body) = scope.clone().unbind();
+                eval(&subst(&body, &name, &eval(arg)))
+            },
+            _ => expr.clone(),
+        },
+    }
+}
+
 pub fn check(context: &Context, expr: &Rc<Expr>, expected_ty: &Rc<Type>) -> Result<(), String> {
     match (&**expr, &**expected_ty) {
         (&Expr::Lam(ref scope), &Type::Arrow(ref param_ty, ref ret_ty)) => {
